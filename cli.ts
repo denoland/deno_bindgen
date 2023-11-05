f3dddec8bba6d6d9254d159eee73158b20300236
Natoandro <anatoandro@hotmail.com>
2023-11-05 19:46:53 +0300

    feat: Add logging (#129)

--- cli.ts
+++ cli.ts
@@ -18,7 +18,11 @@ function build() {
   const args = ["build"];
   if (release) args.push("--release");
   args.push(...flags["--"]);
-  const proc = new Deno.Command("cargo", { args });
+  console.log(
+    "[deno_bindgen] Command:",
+    ["cargo", ...args].map((s) => JSON.stringify(s)).join(" "),
+  );
+  const proc = new Deno.Command("cargo", { args, stderr: "inherit" });
   return proc.output();
 }
 
@@ -38,6 +42,7 @@ async function generate() {
   try {
     conf = JSON.parse(await Deno.readTextFile(metafile));
   } catch (_) {
+    console.log(`[deno_bindgen] metafile not found at '${metafile}'`);
     // Nothing to update.
     return;
   }
@@ -74,13 +79,21 @@ try {
   // no op
 }
 
+console.log("[deno_bindgen] Building...");
 const status = await build().catch((_) => Deno.removeSync(metafile));
 if (status?.success || typeof flags.release == "string") {
   await generate();
   if (source) {
     await ensureDir("bindings");
     await Deno.writeTextFile("bindings/bindings.ts", source);
+    console.log("[deno_bindgen] Written at 'bindings/bindings.ts'");
+  } else {
+    console.log(
+      "[deno_bindgen] bindings.ts not generated: nothing to generate",
+    );
   }
+} else {
+  console.log("[deno_bindgen] Build failed with code", status?.code);
 }
 
 Deno.exit(status?.code || 0);
